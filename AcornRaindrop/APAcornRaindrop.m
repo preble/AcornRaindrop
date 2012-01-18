406f5ad7df1ecb32a5c0291cc6b0c9da7fd1d1e4
preble <preble@gmail.com>
2012-01-18 00:39:00 -0500

    Use the base name of the Acorn image if possible, instead of AcornImage.

--- AcornRaindrop/APAcornRaindrop.m
+++ AcornRaindrop/APAcornRaindrop.m
@@ -26,19 +26,34 @@
 
 - (NSString *)pasteboardNameForTriggeredRaindrop
 {
-	NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"AcornImage.png"];
-	NSURL *fileURL = [NSURL fileURLWithPath:path];
-	
-	// Delete the file, if it already exists, so that we can test for its existence later:
-	[[NSFileManager defaultManager] removeItemAtURL:fileURL error:nil];
-    
+	// Connect to Acorn using JSTalk:
     AcornApp *acorn = (id)[[NSConnection connectionWithRegisteredName:@"com.flyingmeat.Acorn.JSTalk" host:0x00] rootProxy];
     
     if (!acorn) { // Acorn isn't running?
         return nil;
     }
     
-    [[acorn currentDocument] webExportWithOptions:[NSDictionary dictionaryWithObjectsAndKeys:(id)kUTTypePNG, @"uti", path, @"file", nil]];
+	AcornDoc *document = [acorn currentDocument];
+	if (!document)
+		return nil; // no document open
+	
+	// We want to find a good name for this file, since CloudApp will show the name to the link visitor.
+	// If the file has been saved, we'll use the base of the filename, otherwise we'll default to AcornImge.
+	NSString *name;
+	NSURL *documentURL = [document fileURL];
+	if (documentURL)
+		name = [[documentURL lastPathComponent] stringByDeletingPathExtension];
+	else
+		name = @"AcornImage"; // Presumably it is an Untitled document.
+	
+	NSString *path = [[NSTemporaryDirectory() stringByAppendingPathComponent:name] stringByAppendingPathExtension:@"png"];
+	NSURL *fileURL = [NSURL fileURLWithPath:path];
+	
+	// Delete the file, if it already exists, so that we can test for its existence later and make sure this is a fresh export:
+	[[NSFileManager defaultManager] removeItemAtURL:fileURL error:nil];
+	
+	// Ask Acorn to export the document:
+    [document webExportWithOptions:[NSDictionary dictionaryWithObjectsAndKeys:(id)kUTTypePNG, @"uti", path, @"file", nil]];
     
 	// Did Acorn export the file? If not, return nil (Raindrop Error).
 	if (![[NSFileManager defaultManager] fileExistsAtPath:[fileURL path]]) {
